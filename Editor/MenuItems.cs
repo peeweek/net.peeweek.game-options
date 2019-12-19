b45763a8aaf5e6fd605ca46d13dc9cb9448a41a7
Thomas ICHÉ <peeweek@gmail.com>
2019-12-19 16:12:14 +0100

    Base Commit

---
+++ Editor/MenuItems.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace GameOptionsUtility.Editor
+{
+    static class MenuItems
+    {
+        [MenuItem("Assets/Create/Default Game Options")]
+        static void CreateDefaultGraphicsOptions()
+        {
+
+            System.IO.Directory.CreateDirectory($"{Application.dataPath}/Resources");
+            var asset = ScriptableObject.CreateInstance<GraphicOptions>();
+            AssetDatabase.CreateAsset(asset, $"Assets/Resources/{nameof(GraphicOptions)}.asset");
+            EditorGUIUtility.PingObject(asset);
+            Selection.activeObject = asset;
+        }
+    }
+}
+
+
---
+++ Runtime/GameOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameOptionsUtility
+{
+    public static class GameOptions
+    {
+        public class Preferences
+        {
+            public const string prefix = "GameOptions.";
+        }
+
+        public static GraphicOptions graphics;
+
+        [RuntimeInitializeOnLoadMethod]
+        static void InitializeGameOptions()
+        {
+            graphics = GraphicOptions.Load();
+            graphics.Apply();
+        }
+    }
+}
+
