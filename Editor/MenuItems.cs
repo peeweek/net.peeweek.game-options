8161198ae97ac26f4bb548493d60e8d44cc1cd0e
Thomas ICHÉ <peeweek@gmail.com>
2019-12-20 15:58:17 +0100

    Audio Options

--- Editor/MenuItems.cs
+++ Editor/MenuItems.cs
@@ -7,13 +7,25 @@ namespace GameOptionsUtility.Editor
 {
     static class MenuItems
     {
-        [MenuItem("Assets/Create/Default Game Options")]
+        [MenuItem("Assets/Create/Default Graphics Options")]
         static void CreateDefaultGraphicsOptions()
         {
 
             System.IO.Directory.CreateDirectory($"{Application.dataPath}/Resources");
             var asset = ScriptableObject.CreateInstance<GraphicOptions>();
             AssetDatabase.CreateAsset(asset, $"Assets/Resources/{nameof(GraphicOptions)}.asset");
+            EditorGUIUtility.PingObject(asset);
+            Selection.activeObject = asset;
+        }
+
+
+        [MenuItem("Assets/Create/Default Audio Options")]
+        static void CreateDefaultAudioOptions()
+        {
+
+            System.IO.Directory.CreateDirectory($"{Application.dataPath}/Resources");
+            var asset = ScriptableObject.CreateInstance<AudioOptions>();
+            AssetDatabase.CreateAsset(asset, $"Assets/Resources/{nameof(AudioOptions)}.asset");
             EditorGUIUtility.PingObject(asset);
             Selection.activeObject = asset;
         }
--- Runtime/GameOptions.cs
+++ Runtime/GameOptions.cs
@@ -12,11 +12,14 @@ namespace GameOptionsUtility
         }
 
         public static GraphicOptions graphics;
+        public static AudioOptions audio;
 
         static GameOptions()
         {
             graphics = GraphicOptions.Load();
             graphics.Apply();
+            audio = AudioOptions.Load();
+            audio.Apply();
         }
     }
 }
